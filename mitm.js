cd23910f3ac71747aedad3da227e1421e95cbd2a
karimpwnz <karim@karimrahal.com>
2021-07-05 20:37:50 +0300

    Launch

---
+++ mitm.js
@@ -0,0 +1,54 @@
+#!/usr/local/bin/node
+
+const fs = require("fs");
+const child_process = require("child_process");
+
+function getConfigs() {
+    let data = fs.readFileSync("configs.json");
+    return JSON.parse(data);
+}
+
+function log(extensionName, content) {
+    fs.mkdirSync("logs", { recursive: true });
+    fs.writeFile(
+        `logs/${extensionName}.log`,
+        content,
+        { flag: "a+" },
+        (_) => {}
+    );
+}
+
+function extractStreamString(stream) {
+    let chunks = [];
+    let chunk = null;
+    while ((chunk = stream.read()) !== null) {
+        chunks.push(chunk);
+    }
+    return Buffer.concat(chunks);
+}
+
+(() => {
+    if (process.argv.length < 3) {
+        console.error("Error: must be called by a browser extension!");
+        process.exit(1);
+    }
+    // Get extension info
+    let origin = process.argv[2];
+    let extensionConfig = getConfigs()[origin];
+    let extensionName = extensionConfig.name;
+    let redirectScript = extensionConfig.originalScript;
+    // Spawn redirect script process
+    let p = child_process.spawn(redirectScript);
+    // Start logging
+    let i = 0;
+    process.stdin.on("readable", () => {
+        let input = extractStreamString(process.stdin);
+        log(extensionName, `[${++i}] → (CLIENT): ${input}\n\n`);
+        p.stdin.write(input);
+    });
+    p.stdout.on("readable", () => {
+        let output = extractStreamString(p.stdout);
+        log(extensionName, `[${++i}] ← (SERVER): ${output}\n\n`);
+        process.stdout.write(output);
+    });
+})();
